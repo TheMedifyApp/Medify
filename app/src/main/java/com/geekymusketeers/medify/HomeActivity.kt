d30774356c2b1aa47b3acdd558502af45d904883
Binay Shaw <binayshaw7777@gmail.com>
2022-08-31 11:04:11 +0530

    (+) Home Fragments + UI minor changes

--- app/src/main/java/com/geekymusketeers/medify/HomeActivity.kt
+++ app/src/main/java/com/geekymusketeers/medify/HomeActivity.kt
@@ -2,10 +2,31 @@ package com.geekymusketeers.medify
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.geekymusketeers.medify.databinding.ActivityHomeBinding
 
 class HomeActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityHomeBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        //Hides action bar
+        supportActionBar?.hide()
+
+        val bottomNavigationView = binding.bottomNav
+        val navController: NavController = findNavController(R.id.fragmentContainerView)
+        val appBarConfiguration =
+            AppBarConfiguration(setOf(R.id.home, R.id.reminder, R.id.settings))
+        setupActionBarWithNavController(navController, appBarConfiguration)
+
+        bottomNavigationView.setupWithNavController(navController)
     }
 }
