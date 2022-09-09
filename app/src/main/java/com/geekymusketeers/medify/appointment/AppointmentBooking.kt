1ce4cf61cb38075786eedd131348223f1a0e8718
Binay Shaw <binayshaw7777@gmail.com>
2022-09-09 11:11:02 +0530

    Appointment Algorithms designed and implemented

--- app/src/main/java/com/geekymusketeers/medify/appointment/AppointmentBooking.kt
+++ app/src/main/java/com/geekymusketeers/medify/appointment/AppointmentBooking.kt
@@ -3,9 +3,10 @@ package com.geekymusketeers.medify.appointment
 import android.content.Context
 import android.content.Intent
 import android.content.SharedPreferences
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.*
+import android.widget.ArrayAdapter
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.geekymusketeers.medify.R
 import com.geekymusketeers.medify.databinding.ActivityAppointmentBookingBinding
 import com.google.android.material.datepicker.MaterialDatePicker
@@ -15,7 +16,6 @@ import com.google.firebase.database.FirebaseDatabase
 import com.ncorti.slidetoact.SlideToActView
 import java.text.SimpleDateFormat
 import java.util.*
-import kotlin.collections.HashMap
 
 class AppointmentBooking : AppCompatActivity() {
 
@@ -31,28 +31,24 @@ class AppointmentBooking : AppCompatActivity() {
         sharedPreference = baseContext.getSharedPreferences("UserData", Context.MODE_PRIVATE)
         val diseaseValue = HashMap<String, Int>()
         val conditionValue = HashMap<String, Int>()
-        var TotalPoint = 0
+        var totalPoint = 0
 
-        conditionValue["Critical"] = 10
-        conditionValue["Urgent"] = 8
-        conditionValue["Under-control"] = 5
+        conditionValue["Severe Pain"] = 15
+        conditionValue["Mild Pain"] = 8
+        conditionValue["No Pain"] = 0
 
         diseaseValue["Not sure"] = 10
         diseaseValue["Fever"] = 7
-        diseaseValue["Cold & Flu"] = 6
+        diseaseValue["Cold & Flu"] = 4
         diseaseValue["Diarrhea"] = 6
         diseaseValue["Allergies"] = 5
-        diseaseValue["Stomach Aches"] = 6
+        diseaseValue["Stomach Aches"] = 7
         diseaseValue["Conjunctivitis"] = 5
         diseaseValue["Dehydration"] = 4
-        diseaseValue["Tooth ache"] = 6
+        diseaseValue["Tooth ache"] = 7
         diseaseValue["Ear ache"] = 5
-        diseaseValue["Food poisoning"] = 4
-
-
-
-
-
+        diseaseValue["Food poisoning"] = 6
+        diseaseValue["Headache"] = 5
 
 
         val doctorUid = intent.extras!!.getString("Duid")
@@ -167,8 +163,13 @@ class AppointmentBooking : AppCompatActivity() {
                 val time = binding.selectTime.text.toString()
                 val disease = binding.diseaseDropdown.text.toString()
                 val situation = binding.situationDropdown.text.toString()
-                TotalPoint += diseaseValue[disease]!!
-                TotalPoint += conditionValue[situation]!!
+                val rightNow = Calendar.getInstance()
+                val currentHourIn24Format: Int =rightNow.get(Calendar.HOUR_OF_DAY)
+                val firstComeFirstServe = 1 + (0.1 * ((currentHourIn24Format / 10) + 1))
+
+                totalPoint += diseaseValue[disease]!!
+                totalPoint += conditionValue[situation]!!
+                totalPoint = (totalPoint * firstComeFirstServe).toInt()
 
                 val appointmentD:HashMap<String,String> = HashMap() //define empty hashmap
                 appointmentD["PatientName"] = userName
@@ -178,7 +179,7 @@ class AppointmentBooking : AppCompatActivity() {
                 appointmentD["Disease"] = disease
                 appointmentD["PatientCondition"] = situation
                 appointmentD["Prescription"] = userPrescription
-                appointmentD["TotalPoints"] = TotalPoint.toString().trim()
+                appointmentD["TotalPoints"] = totalPoint.toString().trim()
 
                 val appointmentP : HashMap<String, String> = HashMap() //define empty hashmap
                 appointmentP["DoctorUID"] = doctorUid.toString()
@@ -207,12 +208,12 @@ class AppointmentBooking : AppCompatActivity() {
 
 
         // Disease List
-        val items = listOf("Not sure", "Fever", "Cold & Flu", "Diarrhea", "Allergies", "Stomach Aches","Conjunctivitis", "Dehydration", "Tooth ache", "Ear ache", "Food poisoning")
+        val items = listOf("Not sure", "Fever", "Cold & Flu", "Diarrhea","Headache", "Allergies", "Stomach Aches","Conjunctivitis", "Dehydration", "Tooth ache", "Ear ache", "Food poisoning")
         val adapter = ArrayAdapter(this, R.layout.list_items, items)
         binding.diseaseDropdown.setAdapter(adapter)
 
         // Situation List
-        val situationItems = listOf("Critical", "Urgent", "Under-control")
+        val situationItems = listOf("Severe Pain", "Mild Pain", "No Pain")
         val situationAdapter = ArrayAdapter(this, R.layout.list_items, situationItems)
         binding.situationDropdown.setAdapter(situationAdapter)
     }
