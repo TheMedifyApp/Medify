06a07b22fbe0845f19b2dcdf77e0a2cee4f9f76f
Binay Shaw <binayshaw7777@gmail.com>
2022-09-12 18:51:12 +0530

    Date selector for Appointment List

--- app/src/main/java/com/geekymusketeers/medify/mainFragments/PatientAppointmentFragment.kt
+++ app/src/main/java/com/geekymusketeers/medify/mainFragments/PatientAppointmentFragment.kt
@@ -16,9 +16,13 @@ import androidx.recyclerview.widget.RecyclerView
 import com.geekymusketeers.medify.appointment.PatientAppointment
 import com.geekymusketeers.medify.adapter.PatientAppointmentAdapter
 import com.geekymusketeers.medify.databinding.FragmentPatientAppointmentBinding
+import com.google.android.material.datepicker.MaterialDatePicker
 import com.google.firebase.database.*
+import java.text.SimpleDateFormat
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
+import java.util.*
+import kotlin.collections.ArrayList
 
 
 class PatientAppointmentFragment : Fragment() {
@@ -52,19 +56,47 @@ class PatientAppointmentFragment : Fragment() {
         Recyclerview.layoutManager = LinearLayoutManager(requireActivity())
         Recyclerview.setHasFixedSize(true)
 
-        getData()
+        binding.selectDate.setOnClickListener {
+            val datePicker = MaterialDatePicker.Builder.datePicker().build()
+            fragmentManager?.let { datePicker.show(it, "DatePicker") }
+            val date: StringBuilder = StringBuilder("")
+
+            // Setting up the event for when ok is clicked
+            datePicker.addOnPositiveButtonClickListener {
+                // formatting date in dd-mm-yyyy format.
+                val dateFormatter = SimpleDateFormat("dd-MM-yyyy")
+                val tempDate = dateFormatter.format(Date(it)).toString().trim()
+                date.setLength(0)
+                date.append(tempDate)
+                appointmentList.clear()
+                getData(date.toString())
+
+            }
+
+            // Setting up the event for when cancelled is clicked
+            datePicker.addOnNegativeButtonClickListener {
+                Toast.makeText(requireActivity(), "${datePicker.headerText} is cancelled", Toast.LENGTH_LONG).show()
+            }
+
+            // Setting up the event for when back button is pressed
+            datePicker.addOnCancelListener {
+                Toast.makeText(requireActivity(), "Date Picker Cancelled", Toast.LENGTH_LONG).show()
+            }
+        }
+
+//        getData()
 
 
         return binding.root
     }
 
     @RequiresApi(Build.VERSION_CODES.O)
-    private fun getData() {
+    private fun getData(date: String) {
 
-        val current = LocalDateTime.now()
+//        val current = LocalDateTime.now()
         val userID = sharedPreference.getString("uid","Not found").toString()
-        val formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy")
-        val date = current.format(formatter)
+//        val formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy")
+//        val date = current.format(formatter)
 
         dbref = FirebaseDatabase.getInstance().getReference("Users").child(userID).child("PatientsAppointments").child(date)
 
@@ -84,6 +116,9 @@ class PatientAppointmentFragment : Fragment() {
                     error.message, Toast.LENGTH_SHORT).show()
             }
         })
+
+        binding.selectDateTextToHide.visibility = View.GONE
+
     }
 
     override fun onStart() {
