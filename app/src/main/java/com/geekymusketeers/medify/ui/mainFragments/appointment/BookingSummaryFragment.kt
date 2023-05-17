ef3d68580d3f6abf466dffd8ee5d14ea880ba971
Binay Shaw <binayshaw7777@gmail.com>
2023-05-17 12:07:32 +0530

    Completed Appointment Flow

--- app/src/main/java/com/geekymusketeers/medify/ui/mainFragments/appointment/BookingSummaryFragment.kt
+++ app/src/main/java/com/geekymusketeers/medify/ui/mainFragments/appointment/BookingSummaryFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import com.geekymusketeers.medify.R
 import com.geekymusketeers.medify.databinding.FragmentBookingSummaryBinding
@@ -14,6 +15,7 @@ class BookingSummaryFragment : Fragment() {
 
     private lateinit var _binding: FragmentBookingSummaryBinding
     private val binding get() = _binding
+    private val args: BookingSummaryFragmentArgs by navArgs()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -27,7 +29,19 @@ class BookingSummaryFragment : Fragment() {
     }
 
     private fun initView() {
-
+        binding.apply {
+            summaryDoctorName.text = "Doctor's Name: ${args.summary.doctorName}"
+            summaryDoctorSpeciality.text = "Speciality: ${args.summary.doctorSpeciality}"
+            summaryDoctorEmail.text = "Doctor's Email: ${args.summary.doctorEmail}"
+            summaryDoctorPhoneNumber.text = "Doctor's Phone ${args.summary.doctorPhone}"
+            summaryDate.text = "Appointment Date: ${args.summary.appointmentDate}"
+            summaryTime.text = "Appointment Time: ${args.summary.appointmentTime}"
+            summaryDisease.text = "Disease: ${args.summary.disease}"
+            summaryPainLevel.text = "Pain: ${args.summary.painLevel}"
+            btnHome.setOnClickListener {
+                findNavController().popBackStack()
+            }
+        }
     }
 
 }
