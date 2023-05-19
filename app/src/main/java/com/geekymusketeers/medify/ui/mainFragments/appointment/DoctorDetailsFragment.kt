6d1858643907d6d890a626332a4233a3f7e4254c
Aritra Das <aritrarick2002@gmail.com>
2023-05-19 15:48:05 +0530

    Made some changes in DoctorDetails ViewModel.

--- app/src/main/java/com/geekymusketeers/medify/ui/mainFragments/appointment/DoctorDetailsFragment.kt
+++ app/src/main/java/com/geekymusketeers/medify/ui/mainFragments/appointment/DoctorDetailsFragment.kt
@@ -5,10 +5,12 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import com.geekymusketeers.medify.R
+import com.geekymusketeers.medify.base.ViewModelFactory
 import com.geekymusketeers.medify.databinding.FragmentDoctorDetailsBinding
 import com.geekymusketeers.medify.model.User
 import com.geekymusketeers.medify.utils.Logger
@@ -19,7 +21,7 @@ import com.google.android.material.bottomnavigation.BottomNavigationView
 class DoctorDetailsFragment : Fragment() {
 
     private var _binding: FragmentDoctorDetailsBinding? = null
-    private lateinit var detailsViewModel: DoctorDetailsViewModel
+    private val detailsViewModel by viewModels<DoctorDetailsViewModel> { ViewModelFactory() }
     private val binding get() = _binding!!
     private val args: DoctorDetailsFragmentArgs by navArgs()
     private var doctor: User = User()
@@ -31,7 +33,6 @@ class DoctorDetailsFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         _binding = FragmentDoctorDetailsBinding.inflate(inflater, container, false)
-        detailsViewModel = ViewModelProvider(this)[DoctorDetailsViewModel::class.java]
 
         initDoctor()
         initViews()
--- app/src/main/java/com/geekymusketeers/medify/ui/mainFragments/appointment/DoctorDetailsViewModel.kt
+++ app/src/main/java/com/geekymusketeers/medify/ui/mainFragments/appointment/DoctorDetailsViewModel.kt
@@ -1,9 +1,11 @@
 package com.geekymusketeers.medify.ui.mainFragments.appointment
 
+import android.app.Application
 import androidx.lifecycle.ViewModel
+import com.geekymusketeers.medify.base.BaseViewModel
 import com.geekymusketeers.medify.model.User
 
-class DoctorDetailsViewModel : ViewModel() {
+class DoctorDetailsViewModel(application: Application) : BaseViewModel(application) {
 
     private var doctor: User = User()
 
