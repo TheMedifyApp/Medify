f2aef80ee3cf1f3b0a51f8ea66fadd03c76be2af
Aritra Das <aritrarick2002@gmail.com>
2023-05-20 12:57:43 +0530

    Created model for the Stats data.

---
+++ app/src/main/java/com/geekymusketeers/medify/model/Statistics.kt
@@ -0,0 +1,8 @@
+package com.geekymusketeers.medify.model
+
+data class Statistics(
+    var testName: String? = null,
+    var testResult: String? = null,
+    var testDate_Time: String? = null,
+    var testDataRange: String? = null,
+)
